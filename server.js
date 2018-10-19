d35e63e51069a3d6af5826f7828d0bfec356f37e
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2018-10-19 11:33:26 -0300

    fixed import screen

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use(cors());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-app.get('/', function (req, res) {
+
+	app.get('/', function (req, res) {
 	res.render('index.html');
 });
+
 app.post('/import', function(req,res){
 		
 		config.add("threescale:access_token",req.body.access_token);
@@ -35,7 +37,7 @@ app.post('/import', function(req,res){
 		console.log("FILE: " + req.body.swagger_file);
 		config.add("threescale:API","https://"+config.get("threescale:id")+"-admin." + config.get("threescale:wildcard") + "/admin/api"); 
 		swagger.import(req.body.swagger_file, null ,null,null,res);
-		res.write('<html lang="en"><head>  <meta charset="utf-8">  <meta http-equiv=\"X-UA-Compatible" content="IE=edge,chrome=1">  <meta name="viewport" content="width=device-width, initial-scale=1">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <title>3Scale Import</title></head><body>   <img src="3scale-by-redhat.png" style="width:15%;height:15%;"/>   <h1>Swagger Import Tool (non-official)</h1>');
+		res.write('<html lang="en"><head>  <meta charset="utf-8">  <meta http-equiv=\"X-UA-Compatible" content="IE=edge,chrome=1">  <meta name="viewport" content="width=device-width, initial-scale=1">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <title>3Scale Import</title></head><body>  <h1>Swagger Import Tool (non-official)</h1>');
 
 	});
 
